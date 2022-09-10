6e53a228fa96ef1603a7812acf00c5921ca05a8b
Mayank Sood <mayan@Mayanks-MacBook-Pro.local>
2022-09-10 18:26:04 -0400

    Added info and instructions

--- yoga-detector/src/components/HomePage/HomePage.jsx
+++ yoga-detector/src/components/HomePage/HomePage.jsx
@@ -21,11 +21,29 @@ const Home = () => {
                     <h2>Submit your Pose</h2>
                     <form className="form">
                         <ImageCapture/>
-                        <input type="text" placeholder="Name of Pose" onChange={(e) => setPoseName(e.target.value)} />
+                        <input type="text" className='btn btn-success' placeholder="Name of Pose" onChange={(e) => setPoseName(e.target.value)} />
                         <button type="submit" className='btn btn-danger' onClick={(e) => submitForm(e)}>Submit Pose</button>
                     </form>
                 </div>
             </div>
+                
+                
+            <div className='box instructions'>
+                    <h3>Instructions</h3>
+                    
+                    <ul>
+                        <li>Allow the browser access to your webcam</li>
+                        <li>Click 'Capture'</li>
+                        <li>Stand back to ensure your whole body (tip to toe) is clearly visible </li>
+                        <li>Do a yoga pose and see what happens!</li>
+                    </ul>
+                </div>
+
+                <div className='box about'>
+                    <h3>About</h3>
+                   <p>This app uses a machine learning nueral network to detect your yoga poses and idientifies the yoga pose and gives you a score based on the accuracy of the pose in a percentage</p>
+                </div>
+                
         </div>
     )
 }
