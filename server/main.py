0aaf3fdeb9a38df500faf8edd6bd6418721039da
asahapde <60831459+asahapde@users.noreply.github.com>
2022-09-10 21:38:53 -0400

    add image classifier

---
+++ server/main.py
@@ -0,0 +1,124 @@
+import mediapipe as mp
+import cv2
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=2)
+
+
+def detectPose(image, pose):
+
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    results = pose.process(imageRGB)
+    
+    height, width, _ = image.shape
+    
+    landmarks = []
+    
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
+                                  connections=mp_pose.POSE_CONNECTIONS)
+        
+        for landmark in results.pose_landmarks.landmark:
+            landmarks.append((int(landmark.x * width), int(landmark.y * height),
+                                  (landmark.z * width)))
+    
+    return landmarks
+
+def calculateAngle(landmark1, landmark2, landmark3):
+
+    x1, y1, _ = landmark1
+    x2, y2, _ = landmark2
+    x3, y3, _ = landmark3
+ 
+    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
+    
+    if angle < 0:
+ 
+        angle += 360
+    
+    return angle
+
+
+def classifyPose(landmarks):
+    label = 'Unknown Pose'
+    # Calculate the required angles.
+    
+    # Angle between the left shoulder, elbow and wrist points. 
+    left_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                      landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value])
+    
+    # Angle between the right shoulder, elbow and wrist points. 
+    right_elbow_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value],
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value])   
+    
+    # Angle between the left elbow, shoulder and hip points. 
+    left_shoulder_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value],
+                                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value],
+                                         landmarks[mp_pose.PoseLandmark.LEFT_HIP.value])
+ 
+    # Angle between the right hip, shoulder and elbow points. 
+    right_shoulder_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value],
+                                          landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value])
+ 
+    # Angle between the left hip, knee and ankle points. 
+    left_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.LEFT_HIP.value],
+                                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value],
+                                     landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value])
+ 
+    # Angle between the right hip, knee and ankle points 
+    right_knee_angle = calculateAngle(landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value],
+                                      landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value],
+                                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value])
+    
+    #----------------------------------------------------------------------------------------------------------------
+    
+    # warrior II pose or the T pose.
+
+    # both arms are straight.
+    if left_elbow_angle > 165 and left_elbow_angle < 195 and right_elbow_angle > 165 and right_elbow_angle < 195:
+        # shoulders are at the required angle.
+        if left_shoulder_angle > 80 and left_shoulder_angle < 110 and right_shoulder_angle > 80 and right_shoulder_angle < 110:
+ 
+            # warrior II pose.
+            # one leg is straight.
+            if left_knee_angle > 165 and left_knee_angle < 195 or right_knee_angle > 165 and right_knee_angle < 195:
+                # the other leg is bended at the required angle.
+                if left_knee_angle > 90 and left_knee_angle < 120 or right_knee_angle > 90 and right_knee_angle < 120:
+                    label = 'Warrior II Pose' 
+                        
+            # T pose
+            # both legs are straight
+            if left_knee_angle > 160 and left_knee_angle < 195 and right_knee_angle > 160 and right_knee_angle < 195:
+                label = 'T Pose'
+ 
+    
+    # Tree pose
+    # one leg is straight
+    if left_knee_angle > 165 and left_knee_angle < 195 or right_knee_angle > 165 and right_knee_angle < 195:
+        # the other leg is bended at the required angle.
+        if left_knee_angle > 315 and left_knee_angle < 335 or right_knee_angle > 25 and right_knee_angle < 45:
+            label = 'Tree Pose'
+                
+    
+    return label
+
+
+
+# Test
+image = cv2.imread('E:\Hackathons\ShellHacks2022\server\poses\img.jpg')
+landmarks = detectPose(image, pose, display=False)
+
+if landmarks:
+    print(classifyPose(landmarks))
